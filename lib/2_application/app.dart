34a632f07831a651ab90609758c67880b4b08ee3
Joseba Rodriguez <static.jrc@gmail.com>
2023-11-10 16:43:43 +0100

    feat: adds empty HomePage

--- lib/2_application/app.dart
+++ lib/2_application/app.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:ludoteca/2_application/pages/home/home_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const HomePage();
   }
 }
