a9e1cb475e5cee6ff6814a2e10058053987220f1
Joseba Rodriguez <static.jrc@gmail.com>
2023-11-14 11:29:35 +0100

    feat: adds a collection page and navigation for it

--- test/2_application/app_test.dart
+++ test/2_application/app_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ludoteca/2_application/app.dart';
+import 'package:ludoteca/2_application/pages/collection/collection_page.dart';
 import 'package:ludoteca/2_application/pages/home/home_page.dart';
 import 'package:ludoteca/2_application/pages/pages_shell.dart';
 
@@ -10,9 +11,49 @@ void main() {
     expect(find.byType(PagesShell), findsOneWidget);
   });
 
-  testWidgets('should route to HomePage by default', (WidgetTester tester) async {
-    await tester.pumpWidget(const App());
+  group('navigation', () {
+    testWidgets('should route to HomePage by default',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(const App());
 
-    expect(find.byType(HomePage), findsOneWidget);
+      expect(find.byType(HomePage), findsOneWidget);
+    });
+
+    testWidgets('on collection click goes to collection page',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(const App());
+
+      expect(find.byType(CollectionPage), findsNothing);
+
+      final collectionNavigationText = find.text('collection');
+
+      expect(collectionNavigationText, findsOneWidget);
+
+      await tester.tap(collectionNavigationText);
+
+      await tester.pumpAndSettle();
+
+      expect(find.byType(CollectionPage), findsOneWidget);
+    });
+
+    testWidgets('can return to home page', (WidgetTester tester) async {
+      await tester.pumpWidget(const App());
+
+      final collectionNavigationText = find.text('collection');
+
+      await tester.tap(collectionNavigationText);
+
+      await tester.pumpAndSettle();
+
+      expect(find.byType(CollectionPage), findsOneWidget);
+
+      final homeNavigationText = find.text('home');
+
+      await tester.tap(homeNavigationText);
+
+      await tester.pumpAndSettle();
+
+      expect(find.byType(HomePage), findsOneWidget);
+    });
   });
 }
