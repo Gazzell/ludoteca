c2c6eeddb53ee9a2e6e490ed0a2c411e355b9c3c
Joseba Rodriguez <static.jrc@gmail.com>
2023-11-17 17:56:36 +0100

    chore: adds getCollectionItems use case

--- lib/0_data/repositories/collection_repository_mock.dart
+++ lib/0_data/repositories/collection_repository_mock.dart
@@ -1,4 +1,5 @@
 import 'package:either_dart/either.dart';
+import 'package:ludoteca/1_domain/entities/item.dart';
 import 'package:ludoteca/1_domain/entities/unique_id.dart';
 import 'package:ludoteca/1_domain/failures/failures.dart';
 import 'package:ludoteca/1_domain/repositories/collection_repository.dart';
@@ -9,10 +10,29 @@ class CollectionRepositoryMock implements CollectionRepository {
     (index) => ItemId.fromUniqueString('item-$index'),
   );
 
+  late Map<String, Item> _itemCollection;
+  CollectionRepositoryMock() {
+    _itemCollection = {
+      for (var element in _itemIds)
+        element.value: Item(
+          id: element,
+          title: 'Title of ${element.value}',
+          description: 'A super fancy description of ${element.value}',
+          imageUrl:
+              'https://cf.geekdo-images.com/x3zxjr-Vw5iU4yDPg70Jgw__imagepagezoom/img/7a0LOL48K-7JNIOSGtcsNsIxkN0=/fit-in/1200x900/filters:no_upscale():strip_icc()/pic3490053.jpg',
+        ),
+    };
+  }
+
   @override
   Future<Either<Failure, List<ItemId>>> readItemIds() {
     return Future.value(
       Right(_itemIds),
     );
+  }
+
+  @override
+  Future<Either<Failure, List<Item>>> readItems() {
+    return Future.value(Right(_itemCollection.values.toList()));
   }
 }
--- test/1_domain/use_cases/get_collection_items_test.dart
+++ test/1_domain/use_cases/get_collection_items_test.dart
@@ -1,68 +1,73 @@
 import 'package:either_dart/either.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:ludoteca/1_domain/entities/item.dart';
 import 'package:ludoteca/1_domain/entities/unique_id.dart';
 import 'package:ludoteca/1_domain/failures/failures.dart';
 import 'package:ludoteca/1_domain/repositories/collection_repository.dart';
-import 'package:ludoteca/1_domain/use_cases/get_collection_item_ids.dart';
+import 'package:ludoteca/1_domain/use_cases/get_collection_items.dart';
 import 'package:ludoteca/1_domain/use_cases/use_case.dart';
 import 'package:mocktail/mocktail.dart';
 
 class CollectionRepositoryMock extends Mock implements CollectionRepository {}
 
-final collectionIds = List<ItemId>.generate(
+final collectionItems = List<Item>.generate(
   5,
-  (index) => ItemId.fromUniqueString('item-$index'),
+  (index) => Item(
+    id: ItemId.fromUniqueString(index.toString()),
+    title: 'title',
+    description: 'description',
+  ),
 );
 
 void main() {
-  group('GetCollectionItemIds use case', () {
+  group('GetCollectionItems use case', () {
     group('should return Right', () {
-      test('with a list of item ids', () async {
+      test('with a list of items', () async {
         final mockCollectionRepository = CollectionRepositoryMock();
-        when(() => mockCollectionRepository.readItemIds()).thenAnswer(
-          (_) async => Right(collectionIds),
+        when(() => mockCollectionRepository.readItems()).thenAnswer(
+          (_) async => Right(collectionItems),
         );
 
-        final getCollectionItemIdsUseCase = GetCollectionItemIds(
+        final getCollectionItemsUseCase = GetCollectionItems(
           collectionRepository: mockCollectionRepository,
         );
 
-        final result = await getCollectionItemIdsUseCase(NoParams());
+        final result = await getCollectionItemsUseCase(NoParams());
 
-        expect(result, Right<Failure, List<ItemId>>(collectionIds));
+        expect(result, Right<Failure, List<Item>>(collectionItems));
 
-        verify(() => mockCollectionRepository.readItemIds()).called(1);
+        verify(() => mockCollectionRepository.readItems()).called(1);
       });
     });
 
     group('should return Left', () {
       test('with a ServerFailure', () async {
         final mockCollectionRepository = CollectionRepositoryMock();
-        when(() => mockCollectionRepository.readItemIds()).thenAnswer(
+        when(() => mockCollectionRepository.readItems()).thenAnswer(
           (_) async => Left(ServerFailure()),
         );
 
-        final getCollectionItemIdsUseCase = GetCollectionItemIds(
+        final getCollectionItemsUseCase = GetCollectionItems(
           collectionRepository: mockCollectionRepository,
         );
 
-        final result = await getCollectionItemIdsUseCase(NoParams());
+        final result = await getCollectionItemsUseCase(NoParams());
 
         expect(result, Left<Failure, List<ItemId>>(ServerFailure()));
 
-        verify(() => mockCollectionRepository.readItemIds()).called(1);
+        verify(() => mockCollectionRepository.readItems()).called(1);
       });
 
       test('with a ServerFailure if threw an exception', () async {
         final mockCollectionRepository = CollectionRepositoryMock();
-        when(() => mockCollectionRepository.readItemIds())
+        when(() => mockCollectionRepository.readItems())
             .thenThrow(Exception('Something went wrong'));
 
-        final getCollectionItemIdsUseCase = GetCollectionItemIds(
+        final getCollectionItemsUseCase = GetCollectionItems(
           collectionRepository: mockCollectionRepository,
         );
 
-        final result = await getCollectionItemIdsUseCase(NoParams());
+        final result = await getCollectionItemsUseCase(NoParams());
 
         expect(result.isLeft, true);
         expect(
@@ -70,7 +75,7 @@ void main() {
           ServerFailure(stackTrace: 'Exception: Something went wrong'),
         );
 
-        verify(() => mockCollectionRepository.readItemIds()).called(1);
+        verify(() => mockCollectionRepository.readItems()).called(1);
       });
     });
   });
