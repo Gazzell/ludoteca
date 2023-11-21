8e236e709c6823ba1c2cb16ae66fd3e05214c61e
Joseba Rodriguez <static.jrc@gmail.com>
2023-11-21 13:09:06 +0100

    chore: sets url reflects api on goRouter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:ludoteca/2_application/app.dart';
 
 void main() {
+  GoRouter.optionURLReflectsImperativeAPIs = true;
   runApp(const App());
 }
