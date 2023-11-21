c9f24f44b5f407276e352b2ad1a2dfc858d7806b
Joseba Rodriguez <static.jrc@gmail.com>
2023-11-21 18:48:46 +0100

    feat: adds collection item detail cubit

---
+++ lib/2_application/pages/collection/collection_item_detail/cubit/collection_item_detail_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ludoteca/1_domain/entities/item_detail.dart';
+import 'package:ludoteca/1_domain/entities/unique_id.dart';
+import 'package:ludoteca/1_domain/use_cases/get_item_detail.dart';
+import 'package:ludoteca/1_domain/use_cases/use_case.dart';
+
+part 'collection_item_detail_cubit_state.dart';
+
+class CollectionItemDetailCubit extends Cubit<CollectionItemDetailCubitState> {
+  final GetItemDetail getItemDetail;
+  CollectionItemDetailCubit({required this.getItemDetail})
+      : super(const CollectionItemDetailLoadingState());
+
+  Future<void> readItemDetail(ItemId itemId) async {
+    final itemDetail = await getItemDetail(ItemParams(itemId: itemId));
+    if (itemDetail.isLeft) {
+      emit(const CollectionItemDetailErrorState());
+    } else {
+      emit(CollectionItemDetailLoadedState(itemDetail: itemDetail.right));
+    }
+  }
+}
---
+++ lib/2_application/pages/collection/collection_item_detail/cubit/collection_item_detail_cubit_state.dart
@@ -0,0 +1,27 @@
+part of 'collection_item_detail_cubit.dart';
+
+sealed class CollectionItemDetailCubitState extends Equatable {
+  const CollectionItemDetailCubitState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class CollectionItemDetailLoadingState
+    extends CollectionItemDetailCubitState {
+  const CollectionItemDetailLoadingState();
+}
+
+final class CollectionItemDetailErrorState
+    extends CollectionItemDetailCubitState {
+  const CollectionItemDetailErrorState();
+}
+
+final class CollectionItemDetailLoadedState
+    extends CollectionItemDetailCubitState {
+  final ItemDetail itemDetail;
+  const CollectionItemDetailLoadedState({required this.itemDetail}) : super();
+
+  @override
+  List<Object> get props => [itemDetail];
+}
