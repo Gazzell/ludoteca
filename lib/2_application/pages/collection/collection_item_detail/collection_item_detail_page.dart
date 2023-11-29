b6b2c03c2778274c83e45ee7cc1c51880887398a
Joseba Rodriguez <static.jrc@gmail.com>
2023-11-29 19:35:20 +0100

    feat: adds basic collectionItemDetail view states

--- lib/2_application/pages/collection/collection_item_detail/collection_item_detail_page.dart
+++ lib/2_application/pages/collection/collection_item_detail/collection_item_detail_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ludoteca/1_domain/entities/unique_id.dart';
+import 'package:ludoteca/1_domain/repositories/collection_repository.dart';
+import 'package:ludoteca/1_domain/use_cases/get_full_item.dart';
+import 'package:ludoteca/2_application/pages/collection/collection_item_detail/cubit/collection_item_detail_cubit.dart';
 
 class CollectionItemDetailSmallPage extends StatelessWidget {
   final ItemId? selectedItem;
@@ -19,6 +23,31 @@ class CollectionItemDetailSmallPage extends StatelessWidget {
         title: Text(selectedItem?.value ?? ''),
       ),
       body: CollectionItemDetailPage(selectedItem: selectedItem),
+    );
+  }
+}
+
+class CollectionItemDetailPageProvider extends StatelessWidget {
+  final ItemId? itemId;
+  const CollectionItemDetailPageProvider(
+      {super.key, required this.itemId, req});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) {
+        final cubit = CollectionItemDetailCubit(
+          getItemDetail: GetFullItem(
+            collectionRepository:
+                RepositoryProvider.of<CollectionRepository>(context),
+          ),
+        );
+        if (itemId != null) {
+          cubit.readItemDetail(itemId!);
+        }
+        return cubit;
+      },
+      child: CollectionItemDetailPage(selectedItem: itemId),
     );
   }
 }
