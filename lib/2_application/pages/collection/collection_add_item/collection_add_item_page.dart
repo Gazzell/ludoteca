19ef887e42bdbeb92d8a694aad04625dc2217d95
Joseba Rodriguez <static.jrc@gmail.com>
2023-12-29 09:59:18 +0100

    feat: collectionAddItem can find game in bgg

--- lib/2_application/pages/collection/collection_add_item/collection_add_item_page.dart
+++ lib/2_application/pages/collection/collection_add_item/collection_add_item_page.dart
@@ -1,5 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:ludoteca/1_domain/entities/unique_id.dart';
+import 'package:ludoteca/1_domain/repositories/collection_repository.dart';
+import 'package:ludoteca/1_domain/use_cases/get_full_item.dart';
+import 'package:ludoteca/2_application/pages/collection/collection_add_item/cubit/collection_add_item_cubit.dart';
+import 'package:ludoteca/2_application/pages/collection/collection_add_item/view_states/collection_add_item_empty.dart';
+import 'package:ludoteca/2_application/pages/collection/collection_add_item/view_states/collection_add_item_error.dart';
+import 'package:ludoteca/2_application/pages/collection/collection_add_item/view_states/collection_add_item_loaded.dart';
+import 'package:ludoteca/2_application/pages/collection/collection_add_item/view_states/collection_add_item_loading.dart';
+
+class CollectionAddItemPageProvider extends StatelessWidget {
+  final bool showAppBar;
+  final String? title;
+  const CollectionAddItemPageProvider(
+      {super.key, this.showAppBar = false, this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) {
+        return CollectionAddItemCubit(
+          getItemDetail: GetFullItem(
+            collectionRepository:
+                RepositoryProvider.of<CollectionRepository>(context),
+          ),
+        );
+      },
+      child: CollectionAddItemPage(title: title),
+    );
+  }
+}
 
 class CollectionAddItemPage extends StatelessWidget {
   final bool showAppBar;
@@ -9,23 +40,59 @@ class CollectionAddItemPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: showAppBar
-          ? AppBar(
-              title: Text(title ?? ''),
-              leading: IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: () {
-                  context.pop();
-                },
-              ),
-            )
-          : null,
-      body: Builder(
-        builder: (context) {
-          return const Placeholder();
-        },
-      ),
+    return BlocBuilder<CollectionAddItemCubit, CollectionAddItemCubitState>(
+      builder: (context, state) {
+        return Scaffold(
+          backgroundColor: Colors.black54,
+          appBar: showAppBar
+              ? AppBar(
+                  title: Text(title ?? ''),
+                  leading: IconButton(
+                    icon: const Icon(Icons.arrow_back),
+                    onPressed: () {
+                      context.pop();
+                    },
+                  ),
+                )
+              : null,
+          body: Builder(
+            builder: (context) {
+              Widget body;
+              if (state is CollectionAddItemLoadingState) {
+                body = const CollectionAddItemLoading();
+              } else if (state is CollectionAddItemErrorState) {
+                body = const CollectionAddItemError();
+              } else if (state is CollectionAddItemLoadedState) {
+                body = CollectionAddItemLoaded(
+                  key: Key(state.itemDetail.id.value),
+                  itemDetail: state.itemDetail,
+                );
+              } else {
+                body = const CollectionAddItemEmpty();
+              }
+              return SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SearchBar(
+                        leading: const Icon(Icons.search_outlined),
+                        hintText: 'BGG id',
+                        onSubmitted: (value) {
+                          context
+                              .read<CollectionAddItemCubit>()
+                              .readItemDetail(ItemId.fromUniqueString(value));
+                        },
+                      ),
+                    ),
+                    body,
+                  ],
+                ),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
--- test/2_application/pages/collection/collection_add_item/collection_add_item_page_test.dart
+++ test/2_application/pages/collection/collection_add_item/collection_add_item_page_test.dart
@@ -1,37 +1,143 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:ludoteca/1_domain/entities/full_item.dart';
 import 'package:ludoteca/2_application/pages/collection/collection_add_item/collection_add_item_page.dart';
+import 'package:ludoteca/2_application/pages/collection/collection_add_item/cubit/collection_add_item_cubit.dart';
+import 'package:ludoteca/2_application/pages/collection/collection_add_item/view_states/collection_add_item_error.dart';
+import 'package:ludoteca/2_application/pages/collection/collection_add_item/view_states/collection_add_item_loaded.dart';
+import 'package:ludoteca/2_application/pages/collection/collection_add_item/view_states/collection_add_item_loading.dart';
+import 'package:mocktail_image_network/mocktail_image_network.dart';
+
+class MockCollectionAddItemCubit extends MockCubit<CollectionAddItemCubitState>
+    implements CollectionAddItemCubit {}
 
 void main() {
   group('CollectionAddItemPage', () {
-    Widget widgetUnderTest({bool showAppBar = false, String? title}) {
+    late CollectionAddItemCubit mockCollectionAddItemCubit;
+
+    setUp(() => mockCollectionAddItemCubit = MockCollectionAddItemCubit());
+
+    Widget widgetUnderTest({
+      required CollectionAddItemCubit cubit,
+      bool showAppBar = false,
+      String? title,
+    }) {
       return MaterialApp(
-          home: CollectionAddItemPage(
-        showAppBar: showAppBar,
-        title: title,
-      ));
+        home: BlocProvider(
+          create: (context) => cubit,
+          child: CollectionAddItemPage(
+            showAppBar: showAppBar,
+            title: title,
+          ),
+        ),
+      );
     }
 
     testWidgets(
       'renders a CollectionAddItemPage with no header',
       (WidgetTester tester) async {
-        await tester.pumpWidget(widgetUnderTest());
+        whenListen(
+          mockCollectionAddItemCubit,
+          Stream.fromIterable([const CollectionAddItemEmptyState()]),
+          initialState: const CollectionAddItemEmptyState(),
+        );
+
+        await tester
+            .pumpWidget(widgetUnderTest(cubit: mockCollectionAddItemCubit));
 
         expect(find.byType(AppBar), findsNothing);
         expect(find.text('title'), findsNothing);
-        expect(find.byType(Placeholder), findsOneWidget);
+        expect(find.byType(SearchBar), findsOneWidget);
       },
     );
 
     testWidgets(
       'renders a CollectionAddItemPage with a header',
       (WidgetTester tester) async {
-        await tester
-            .pumpWidget(widgetUnderTest(showAppBar: true, title: 'title'));
+        whenListen(
+          mockCollectionAddItemCubit,
+          Stream.fromIterable([const CollectionAddItemEmptyState()]),
+          initialState: const CollectionAddItemEmptyState(),
+        );
+
+        await tester.pumpWidget(widgetUnderTest(
+          cubit: mockCollectionAddItemCubit,
+          showAppBar: true,
+          title: 'title',
+        ));
 
         expect(find.byType(AppBar), findsOneWidget);
         expect(find.text('title'), findsOneWidget);
-        expect(find.byType(Placeholder), findsOneWidget);
+        expect(find.byType(SearchBar), findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      'when [CollectionAddItemLoadingState] renders a CollectionAddItemLoading',
+      (WidgetTester tester) async {
+        whenListen(
+          mockCollectionAddItemCubit,
+          Stream.fromIterable([
+            const CollectionAddItemEmptyState(),
+            const CollectionAddItemLoadingState(),
+          ]),
+          initialState: const CollectionAddItemEmptyState(),
+        );
+
+        await tester
+            .pumpWidget(widgetUnderTest(cubit: mockCollectionAddItemCubit));
+
+        await tester.pump();
+
+        expect(find.byType(CollectionAddItemLoading), findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      'when [CollectionAddItemErrorState] renders a CollectionAddItemError',
+      (WidgetTester tester) async {
+        whenListen(
+          mockCollectionAddItemCubit,
+          Stream.fromIterable([
+            const CollectionAddItemEmptyState(),
+            const CollectionAddItemErrorState(),
+          ]),
+          initialState: const CollectionAddItemEmptyState(),
+        );
+
+        await tester
+            .pumpWidget(widgetUnderTest(cubit: mockCollectionAddItemCubit));
+
+        await tester.pump();
+
+        expect(find.byType(CollectionAddItemError), findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      'with [CollectionAddItemLoadedState] renders a CollectionAddItemLoaded',
+      (WidgetTester tester) async {
+        final fullItem = FullItem.empty();
+        whenListen(
+          mockCollectionAddItemCubit,
+          Stream.fromIterable(
+            [
+              const CollectionAddItemEmptyState(),
+              CollectionAddItemLoadedState(itemDetail: fullItem),
+            ],
+          ),
+          initialState: const CollectionAddItemEmptyState(),
+        );
+
+        await tester.pumpWidget(
+          widgetUnderTest(cubit: mockCollectionAddItemCubit),
+        );
+
+        await mockNetworkImages(() async => tester.pump());
+
+        expect(find.byType(CollectionAddItemLoaded), findsOneWidget);
       },
     );
   });
