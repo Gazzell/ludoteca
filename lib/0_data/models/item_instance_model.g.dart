bb9ea07ddeff07f149368ab4be5b579b93fdf2cf
Joseba Rodriguez <static.jrc@gmail.com>
2024-09-16 11:46:07 +0200

    chore: adds ItemInstanceModel

---
+++ lib/0_data/models/item_instance_model.g.dart
@@ -0,0 +1,36 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'item_instance_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ItemInstanceModel _$ItemInstanceModelFromJson(Map<String, dynamic> json) =>
+    ItemInstanceModel(
+      instanceId: (json['instanceId'] as num).toInt(),
+      itemId: json['itemId'] as String,
+      status: json['status'] as String,
+      incidences: (json['incidences'] as List<dynamic>?)
+              ?.map((e) => e as String)
+              .toList() ??
+          const [],
+      borrowedAt: json['borrowedAt'] == null
+          ? null
+          : DateTime.parse(json['borrowedAt'] as String),
+      returnedAt: json['returnedAt'] == null
+          ? null
+          : DateTime.parse(json['returnedAt'] as String),
+      borrowedBy: json['borrowedBy'] as String?,
+    );
+
+Map<String, dynamic> _$ItemInstanceModelToJson(ItemInstanceModel instance) =>
+    <String, dynamic>{
+      'instanceId': instance.instanceId,
+      'itemId': instance.itemId,
+      'status': instance.status,
+      'incidences': instance.incidences,
+      'borrowedAt': instance.borrowedAt?.toIso8601String(),
+      'returnedAt': instance.returnedAt?.toIso8601String(),
+      'borrowedBy': instance.borrowedBy,
+    };
--- lib/1_domain/entities/item_instance.dart
+++ lib/1_domain/entities/item_instance.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:ludoteca/0_data/models/item_instance_model.dart';
 import 'package:ludoteca/1_domain/entities/unique_id.dart';
 
 enum ItemInstanceStatus {
@@ -24,6 +25,22 @@ class ItemInstance extends Equatable {
     this.returnedAt,
     this.borrowedBy,
   });
+
+  factory ItemInstance.fromItemInstanceModel(ItemInstanceModel itemInstanceModel) {
+    return ItemInstance(
+      instanceId: itemInstanceModel.instanceId,
+      itemId: ItemId.fromUniqueString(itemInstanceModel.itemId),
+      status: itemInstanceModel.status == 'available'
+          ? ItemInstanceStatus.available
+          : ItemInstanceStatus.unavailable,
+      incidences: itemInstanceModel.incidences,
+      borrowedAt: itemInstanceModel.borrowedAt,
+      returnedAt: itemInstanceModel.returnedAt,
+      borrowedBy: itemInstanceModel.borrowedBy != null
+          ? UniqueId.fromUniqueString(itemInstanceModel.borrowedBy!)
+          : null,
+    );
+  }
 
   @override
   List<Object?> get props => [
